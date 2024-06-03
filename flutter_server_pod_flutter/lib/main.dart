b8dfef8df29702037e59e26aeb63f5ea85471d70
KumarSunil <sunil@smartters.in>
2024-06-03 15:37:38 +0530

    wip auth

--- flutter_server_pod_flutter/lib/main.dart
+++ flutter_server_pod_flutter/lib/main.dart
@@ -1,16 +1,17 @@
-import 'package:flutter_server_pod_client/flutter_server_pod_client.dart';
-import 'package:flutter/material.dart';
-import 'package:serverpod_flutter/serverpod_flutter.dart';
-
+ import 'package:flutter/material.dart';
+import 'package:flutter_server_pod_flutter/pages/sign_in/sign_in_page.dart';
+import 'package:flutter_server_pod_flutter/src/serverpod_client.dart';
+ 
 // Sets up a singleton client object that can be used to talk to the server from
 // anywhere in our app. The client is generated from your server code.
 // The client is set up to connect to a Serverpod running on a local server on
 // the default port. You will need to modify this to connect to staging or
 // production servers.
-var client = Client('http://$localhost:8080/')
-  ..connectivityMonitor = FlutterConnectivityMonitor();
+ 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await initializeServerpodClient();
   runApp(const MyApp());
 }
 
@@ -22,9 +23,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Serverpod Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Serverpod Example'),
+      home: const SignInPage(),
     );
   }
 }
